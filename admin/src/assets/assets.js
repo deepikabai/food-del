79332d0b16d0bbdb9894ce610242ac07beb20795
Deepika Bai <deepikamahadevan01@gmail.com>
2024-07-27 02:30:53 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-del-backend-1eeu.onrender.com
+export const url = 'https://food-del-backend-1eeu.onrender.com"
 export const currency = '₹'
 
 export const assets ={
